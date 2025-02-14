ef4bc42bc006671265ec7ae827ddb13ff994335a
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-02-14 12:16:51 +0600

    Login and logout toster notification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,7 +56,12 @@ class UserController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        $notification = array (
+            'message' => 'User Logout successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect('/login')->with($notification);
     }//end
 
     //user update password
