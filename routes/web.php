20997f74ba59f7f0057bcb6a1d78f7a620cac820
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-02-15 14:09:33 +0600

    Admin All_brand and Add_brand page setup

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Backend\BrandController;
 
 
 /*
@@ -76,3 +77,13 @@ Route::middleware(['auth','role:vendor'])->group(function() {
 });
 //vendor login
 Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);
+
+
+Route::middleware(['auth','role:admin'])->group(function() {
+    //Backend all brand
+    Route::controller(BrandController::class)->group(function() {
+        Route::get('all/brand', 'AllBrand')->name('all.brand');
+        Route::get('add/brand', 'AddBrand')->name('add.brand');
+    });
+
+});
