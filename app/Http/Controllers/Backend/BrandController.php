d8691fcf1ac34515d9c1359976914b7b0a61c600
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-02-15 16:10:40 +0600

    Intervention image packege install

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Backend;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Brand;
