f031c7f431c0c96281ba1899dc9ad3be0f074e6e
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-03-04 12:59:32 +0600

    Add Formvalidation on Admin_dashboard's Add product page

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -24,27 +24,29 @@
 				  <div class="card-body p-4">
 					  <h5 class="card-title">Add New Product</h5>
 					  <hr/>
+					  <form id="myForm" method="post" action="{{ route('store.category') }}" enctype="multipart/form-data" >
+                            @csrf  
                        <div class="form-body mt-4">
 					    <div class="row">
 						   <div class="col-lg-8">
                            <div class="border border-3 p-4 rounded">
-							<div class="mb-3">
+							<div class="form-group mb-3">
 								<label for="inputProductTitle" class="form-label">Product Name</label>
 								<input type="text" name="product_name" class="form-control" id="inputProductTitle" placeholder="Enter product name">
 							</div>
-							<div class="mb-3">
+							<div class="form-group mb-3">
 								<label for="inputProductTitle" class="form-label">Product Tags</label>
 								<input type="text" name="product_tags" class="form-control visually-hidden __web-inspector-hide-shortcut__" data-role="tagsinput" value="new product,top product">
 							</div>
-							<div class="mb-3">
+							<div class="form-group mb-3">
 								<label for="inputProductTitle" class="form-label">Product Size</label>
 								<input type="text" name="product_size" class="form-control visually-hidden __web-inspector-hide-shortcut__" data-role="tagsinput" value="Small,Midium,Large">
 							</div>
-							<div class="mb-3">
+							<div class="form-group mb-3">
 								<label for="inputProductTitle" class="form-label">Product Color</label>
 								<input type="text" name="product_color" class="form-control visually-hidden __web-inspector-hide-shortcut__" data-role="tagsinput" value="Red,Blue,Black">
 							</div>
-							<div class="mb-3">
+							<div class="form-group mb-3">
                                 <label for="inputProductDescription" class="form-label">Short Description</label>
                                 <textarea class="form-control" name="short_descp" id="inputProductDescription" rows="3"></textarea>
 							</div>
@@ -52,12 +54,12 @@
                                 <label for="inputProductDescription" class="form-label">Long Description</label>
                                 <textarea id="mytextarea" name="long_descp">Long, Description!</textarea>
 							</div>
-							<div class="mb-3">
+							<div class="form-group mb-3">
 								<label for="inputProductTitle" class="form-label">Main Thambnail</label>
 								<input class="form-control" name="product_thambnail" type="file" id="formFile" onChange="mainThamUrl(this)">
 								<img src="" id="mainThmb" />
 							</div>
-							<div class="mb-3">
+							<div class="form-group mb-3">
 								<label for="inputProductTitle" class="form-label">Multiple Images</label>
 								<input class="form-control" name="multi_img[]" type="file" id="multiImg" multiple="">
 								<div class="row" id="preview_img"></div>
@@ -68,23 +70,23 @@
 						   <div class="col-lg-4">
 							<div class="border border-3 p-4 rounded">
                               <div class="row g-3">
-								<div class="col-md-6">
+								<div class="form-group col-md-6">
 									<label for="inputPrice" class="form-label">Product Price</label>
 									<input type="text" name="selling_price" class="form-control" id="inputPrice" placeholder="00.00">
 								  </div>
-								  <div class="col-md-6">
+								  <div class="form-group col-md-6">
 									<label for="inputCompareatprice" class="form-label">Discount Price</label>
 									<input type="text" name="discount_price" class="form-control" id="inputCompareatprice" placeholder="00.00">
 								  </div>
-								  <div class="col-md-6">
+								  <div class="form-group col-md-6">
 									<label for="inputCostPerPrice" class="form-label">Product Code</label>
 									<input type="text" name="product_code" class="form-control" id="inputCostPerPrice" placeholder="00.00">
 								  </div>
-								  <div class="col-md-6">
+								  <div class="form-group col-md-6">
 									<label for="inputStarPoints" class="form-label">Product Qty</label>
 									<input type="text" name="product_qty" class="form-control" id="inputStarPoints" placeholder="00.00">
 								  </div>
-								  <div class="col-12">
+								  <div class="form-group col-12">
 									<label for="inputProductType" class="form-label">Product Brand</label>
 									<select class="form-select" name="brand_id" id="inputProductType">
 										<option></option>
@@ -93,7 +95,7 @@
 										@endforeach
 									  </select>
 								  </div>
-								  <div class="col-12">
+								  <div class="form-group col-12">
 									<label for="inputVendor" class="form-label">Product Category</label>
 									<select class="form-select" name="category_id" id="inputVendor">
 										<option></option>
@@ -102,7 +104,7 @@
 										@endforeach
 									  </select>
 								  </div>
-								  <div class="col-12">
+								  <div class="form-group col-12">
 									<label for="inputCollection" class="form-label">Product Subcategory</label>
 									<select class="form-select" name="subcategory_id" id="inputCollection">
 										<option></option>
@@ -148,7 +150,7 @@
 									<hr>
 								  <div class="col-12">
 									  <div class="d-grid">
-                                         <button type="button" class="btn btn-primary">Save Product</button>
+										 <input type="submit" class="btn btn-primary px-4" value="Save Changes" />
 									  </div>
 								  </div>
 							  </div> 
@@ -157,12 +159,86 @@
 					   </div><!--end row-->
 					</div>
 				  </div>
+				</form>
 			  </div>
-
 			</div>
 
 
+<!-- form validation -->
+<script type="text/javascript">
+    $(document).ready(function (){
+        $('#myForm').validate({
+            rules: {
+                product_name: {
+                    required : true,
+                }, 
+                 short_descp: {
+                    required : true,
+                }, 
+                 product_thambnail: {
+                    required : true,
+                }, 
+                 multi_img: {
+                    required : true,
+                }, 
+                 selling_price: {
+                    required : true,
+                },                   
+                 product_code: {
+                    required : true,
+                }, 
+                 product_qty: {
+                    required : true,
+                }, 
+                 brand_id: {
+                    required : true,
+                }, 
+                 category_id: {
+                    required : true,
+                }, 
+                 subcategory_id: {
+                    required : true,
+                }, 
+            },
+            messages :{
+                product_name: {
+                    required : 'Please Enter Product Name',
+                },
+                short_descp: {
+                    required : 'Please Enter Short Description',
+                },
+                product_thambnail: {
+                    required : 'Please Select Product Thambnail Image',
+                },
+                multi_img: {
+                    required : 'Please Select Product Multi Image',
+                },
+                selling_price: {
+                    required : 'Please Enter Selling Price',
+                }, 
+                product_code: {
+                    required : 'Please Enter Product Code',
+                },
+                 product_qty: {
+                    required : 'Please Enter Product Quantity',
+                },
 
+            },
+            errorElement : 'span', 
+            errorPlacement: function (error,element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight : function(element, errorClass, validClass){
+                $(element).addClass('is-invalid');
+            },
+            unhighlight : function(element, errorClass, validClass){
+                $(element).removeClass('is-invalid');
+            },
+        });
+    });
+    
+</script>
 
 		<!-- main Thambnail img show script -->
 		<script type="text/javascript">
