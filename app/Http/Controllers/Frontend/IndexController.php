7729ea7063fb2272441dc2ce4ef44e352f377eff
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-05-03 15:06:10 +0600

    Make dynamic frontend categorywise 'category_view' page

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -73,7 +73,10 @@ class IndexController extends Controller
         $products = Product::where('status',1)->where('category_id',$id)->orderBy('id','DESC')->get();
         $categories = Category::orderBy('category_name','ASC')->get();
 
-        return view('frontend.product.category_view', compact('products', 'categories'));
+        $breadcat = Category::where('id',$id)->first();
+        $newProduct = Product::orderBy('id','DESC')->limit(3)->get();
+
+        return view('frontend.product.category_view', compact('products', 'categories', 'breadcat', 'newProduct'));
 
     }
 }
