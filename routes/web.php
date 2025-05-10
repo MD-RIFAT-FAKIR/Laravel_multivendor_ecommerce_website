f6471ce29c4f5f49fd15ccfc44b7d428859769c6
MD-RIFAT-FAKIR <mdrifatfakir001@gmail.com>
2025-05-10 16:03:31 +0600

    bumbummen shoping cart installation

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\BannerController;
 
 use App\Http\Controllers\Frontend\IndexController;
+use App\Http\Controllers\Frontend\CartController;
 
 
 /*
